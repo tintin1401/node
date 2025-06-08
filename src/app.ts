349e9ef4cc8bf1b2dfadf622add3df285cf9f1f7
Dana <dana.cardenascr@gmail.com>
2025-06-08 14:25:36 -0600

    Remove mongo folder from tracking

--- src/app.ts
+++ src/app.ts
@@ -11,15 +11,25 @@ async function main() {
         dbName: 'EntrenoYA'
     });
 
+    /*
+    Create a new log entry
     const newLog = await LogModel.create({
         message: "This is a test log message",
         level: "medium",
         oring: "Backend"
     });
-
+    Wait for the log to be saved
     await newLog.save();
 
     console.log(newLog);
+    Fetch the last 10 logs
+    const logs = await LogModel.find({}).sort({ createdAt: -1 }).limit(10);
+    
+    console.log(logs[0].createdAt);
+
+    */
+
+   
 
 
 }
