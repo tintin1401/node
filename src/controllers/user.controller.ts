25319eadfeeb56f77f6a97e083193ebded24e5af
benjaminpaniagua <benjaminjosepaniaguarojas@gmail.com>
2025-06-16 08:34:06 -0600

    feat: implement user registration functionality and update user routes

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -13,10 +13,32 @@ export const getUsers = async (req: Request, res: Response): Promise<void> => {
     
     if (!user) {
       res.status(404).json({ error: 'Usuario no encontrado' });
-      return;
     }
     res.json(user);
   } catch (error) {
     res.status(500).json({ error: 'Error al buscar el usuario' });
   }
 };
+
+export const register = async (req: Request, res: Response): Promise<void> => {
+    const { fullName, phone, email, password, role, profilePictureUrl } = req.body; 
+
+    try {
+      const newUser = new UserModel({
+        fullName,
+        phone,
+        email,
+        password,
+        role,
+        profilePictureUrl
+      });
+
+      await newUser.save();
+      res.send('Usuario con el nombre ' + fullName + ' guardado correctamente');
+    } catch (error) {
+      console.error('Error al guardar el usuario:', error);
+    }
+};
+
+
+//
