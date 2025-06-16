c8767ffca458c7ccb802e4beb4f13d37cb2a6912
benjaminpaniagua <benjaminjosepaniaguarojas@gmail.com>
2025-06-16 09:05:22 -0600

    feat: update user registration logic and enhance error handling

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -20,25 +20,53 @@ export const getUsers = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
+// export const register = async (req: Request, res: Response): Promise<void> => {
+//     const { fullName, phone, email, password, role, profilePictureUrl } = req.body; 
+
+//     try {
+//       const newUser = new UserModel({
+//         fullName,
+//         phone,
+//         email,
+//         password,
+//         role,
+//         profilePictureUrl
+//       });
+
+//       await newUser.save();
+//       res.send('Usuario con el nombre ' + fullName + ' guardado correctamente');
+//     } catch (error) {
+//       console.error('Error al guardar el usuario:', error);
+//     }
+// };
+
 export const register = async (req: Request, res: Response): Promise<void> => {
-    const { fullName, phone, email, password, role, profilePictureUrl } = req.body; 
-
     try {
-      const newUser = new UserModel({
-        fullName,
-        phone,
-        email,
-        password,
-        role,
-        profilePictureUrl
-      });
-
-      await newUser.save();
-      res.send('Usuario con el nombre ' + fullName + ' guardado correctamente');
+        const registerData = req.body;
+        const newRegister = new UserModel(registerData);
+        await newRegister.save();
+        res.status(201).json(newRegister);
+       res.send('Usuario con el nombre ' + registerData.fullName + ' guardado correctamente');
     } catch (error) {
-      console.error('Error al guardar el usuario:', error);
+        res.status(500).json({ error: 'Error al crear el usuario con nombre ' + req.body.fullName + 'puede ser que ya esté registrado el número telefónico, el correo o falta algún campo.' });
     }
-};
+}
 
+export const isTrainer = async (userData: { fullName: string, email: string, role: string }) => {
+    try {
+        const { fullName, email, role } = userData;
+        const isTrainer = await UserModel.findOne({
+            fullName, 
+            email, 
+            role: 'trainer' // Check if the user is a trainer
+        });
 
-//
+        if (isTrainer) {
+           return false; 
+        } else {
+            return true;
+        }
+    } catch (error) {
+        console.error('Error checking if is trainer:', error);
+    }
+}
--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUsers, register } from '../controllers/user.controller';
+import { getUsers, isTrainer, register } from '../controllers/user.controller';
 
 const router = express.Router();
 
