0badf3873ffe1661364877dec4a1e30bb0452ff3
benjaminpaniagua <135351665+benjaminpaniagua@users.noreply.github.com>
2025-06-17 10:02:02 -0600

    feat: add bcrypt for password encryption and update user registration and login functionality

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,117 @@
 import { Request, Response } from 'express';
 import {UserModel} from "../data/"
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
-// Obtener todos los usuarios
+const saltRounds = 10;
+
+export const encryptPassword = async (plainPassword: string): Promise<string> => {
+  const hash = await bcrypt.hash(plainPassword, saltRounds);
+  return hash;
+};
+
+export const comparePassword = async (plainPassword: string, hashedPassword: string): Promise<boolean> => {
+  const isMatch = await bcrypt.compare(plainPassword, hashedPassword);
+  return isMatch;
+};
+
+export const register = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const registerData = req.body;
+    registerData.password = await encryptPassword(registerData.password); //store the hashed password
+
+    const newRegister = new UserModel(registerData);
+    await newRegister.save();
+
+    res.status(201).json({ message: `Usuario ${registerData.fullName} con rol ${registerData.role} registrado exitosamente`, user: newRegister });
+  } catch (error) {
+    res.status(500).json({
+      error: `Error al crear el usuario con nombre ${req.body.fullName}. Puede que ya esté registrado el número, correo o falte algún campo de completar.`,
+    });
+  }
+};
+
+
+export const login = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      res.status(401).json({ error: 'Usuario no encontrado' });
+      return;
+    }
+
+    const isMatch = await comparePassword(password, user.password);
+    if (!isMatch) {
+      res.status(401).json({ error: 'Credenciales inválidas' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Inicio de sesión exitoso', user });
+
+  } catch (error) {
+    res.status(500).json({ error: 'Error al iniciar sesión' });
+  }
+}
+
+export const updateUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const userId = req.params.id;
+    const updateData = req.body;
+
+    if (updateData.password) {
+      updateData.password = await encryptPassword(updateData.password);
+    }
+
+    const updatedUser = await UserModel.findByIdAndUpdate(userId, updateData, { new: true });
+
+    if (!updatedUser) {
+      res.status(404).json({ error: 'Usuario no encontrado' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Usuario actualizado correctamente', user: updatedUser });
+  } catch (error) {
+    res.status(500).json({ error: 'Error al actualizar el usuario' });
+  }
+};
+
+
+
+// export const updateUser = async(req: Request, res: Response): Promise<void> => {
+//   try {
+//     const email = req.params.email;
+
+//     const updateData = req.body;
+//     const updatedUser = await UserModel.findByIdAndUpdate(email, updateData, { newUser: true });
+
+//     if (!updatedUser) {
+//       res.status(404).json({ error: 'Usuario no encontrado' });
+//       return;
+//     }
+//     res.status(200).json(updatedUser);
+//   } catch ( error ) {
+//     res.status(500).json({ error: 'Error al actualizar el usuario' });
+//   }
+// }
+
+
+export const isTrainer = async ( trainerId: string ) => {
+    try {
+        const objectId = new mongoose.Types.ObjectId(trainerId);
+        const isTrainer = await UserModel.findOne({_id: objectId });
+        if (isTrainer && isTrainer.role == 'trainer') {
+          return true;
+        } else {
+          return false;
+        }
+    } catch (error) {
+        console.error('Error checking if is trainer:', error);
+        return false;
+    }
+}
+
 export const getUsers = async (req: Request, res: Response): Promise<void> => {
  try {
     const { fullName, email } = req.body; // Add more attributes as needed
@@ -21,55 +130,6 @@ export const getUsers = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
-export const register = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const registerData = req.body;
-        if( req.body.role === 'trainer' ) {
-        const newRegister = new UserModel(registerData);
-        await newRegister.save();
-        res.status(201).json(newRegister);
-        res.send('Trainer con el nombre ' + registerData.fullName + ' guardado correctamente');
-        } else if (req.body.role === 'client') {
-          const newRegister = new UserModel(registerData);
-          await newRegister.save();
-       res.send('Usuario con el nombre ' + registerData.fullName + ' guardado correctamente');
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Error al crear el usuario con nombre ' + req.body.fullName + 'puede ser que ya esté registrado el número telefónico, el correo o falta algún campo.' });
-    }
-}
-
-
-
-export const isTrainer = async ( trainerId: string ) => {
-    try {
-        const objectId = new mongoose.Types.ObjectId(trainerId);
-        const isTrainer = await UserModel.findOne({_id: objectId });
-        if (isTrainer && isTrainer.role == 'trainer') {
-          return true;
-        } else {
-          return false;
-        }
-    } catch (error) {
-        console.error('Error checking if is trainer:', error);
-        return false;
-    }
-}
-
-export const login = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const { email, password } = req.body;
-    const user = await UserModel.findOne({ email, password });
-    if (!user) {
-      res.status(401).json({ error: 'Credenciales inválidas' });
-    }
-    res.status(200).json({ message: 'Inicio de sesión exitoso', user });
-
-  } catch (error) {
-    res.status(500).json({ error: 'Error al iniciar sesión' });
-  }
-}
-
 export const getTrainers = async (req: Request, res: Response): Promise<void> => {
   try {
     const trainers = await UserModel.find({ role: 'trainer' });
@@ -79,21 +139,5 @@ export const getTrainers = async (req: Request, res: Response): Promise<void> =>
     res.status(200).json(trainers);
   } catch (error) {
     res.status(500).json({ error: 'Error al obtener los entrenadores' });
-  }
-}
-
-export const updateUser = async(req: Request, res: Response): Promise<void> => {
-  try {
-    const userId = req.params.id;
-
-    const updateData = req.body;
-    const updatedUser = await UserModel.findByIdAndUpdate(userId, updateData, { newUser: true });
-    if (!updatedUser) {
-      res.status(404).json({ error: 'Usuario no encontrado' });
-      return;
-    }
-    res.status(200).json(updatedUser);
-  } catch ( error ) {
-    res.status(500).json({ error: 'Error al actualizar el usuario' });
   }
 }
--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,14 +1,16 @@
 import express from 'express';
-import { getUsers, getTrainers, login, register } from '../controllers/user.controller';
+import { getUsers, getTrainers, updateUser, login, register } from '../controllers/user.controller';
 
 const router = express.Router();
 
-// Example Express route
-router.post('/find', getUsers);
 router.post('/register', register);
-
 router.post('/login', login);
+
+router.put('/updateUser/:id', updateUser);
+
 router.get('/getTrainers', getTrainers);
+router.get('/getUsers', getUsers);
+
 
 
 export default router;
